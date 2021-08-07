43d0e4c45f8bc00da75520e6040394f7d9240a97
Ceren <cerenaltay.lsb@gmail.com>
2021-08-07 14:50:46 +0100

    UI added

---
+++ UI/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <header />
+    </div>
+</template>
+
+<script>
+
+    import Header from './components/Header'
+
+    export default {
+        components: {
+            Header,
+        },
+    };
+</script>
+
+
---
+++ UI/src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import App from './App.vue';
+
+Vue.config.productionTip = true;
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+
+
+
+
+
+
+new Vue({
+    render: h => h(App)
+}).$mount('#app');
