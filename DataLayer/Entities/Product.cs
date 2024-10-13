90944792e3be7c37ec54fc491d962d2b1a9f39bf
Your Name <you@example.com>
2024-10-13 16:34:12 +0100

    Added nullable

--- DataLayer/Entities/Product.cs
+++ DataLayer/Entities/Product.cs
@@ -9,16 +9,16 @@ namespace ECommerceApplication.DataLayer.Entities
 {
    public class Product : DbEntity
     {
-        public string Name { get; set; }
+        public required string Name { get; set; }
 
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
-        public string ImageUrl { get; set; }
+        public string? ImageUrl { get; set; }
 
         public double Price { get; set; }
 
         public int CategoryId { get; set; }
 
-        public virtual Category Category { get; set; }
+        public required virtual Category Category { get; set; }
     }
 }
